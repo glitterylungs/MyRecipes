833e64bf8a133c37aaac26b46413c81aab2b1968
Alicja Gruca <alicjagruca6@gmail.com>
2022-08-24 23:03:54 +0200

    Initial Commit

---
+++ MyRecipes/MyRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipesApp.swift
+//  MyRecipes
+//
+//  Created by Alicja Gruca on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
